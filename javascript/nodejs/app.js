3d245c04b85fd21b970be6bec41748879bdbfc41
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-05-16 23:20:32 -0400

    Simple refactor and add axios

--- javascript/nodejs/app.js
+++ javascript/nodejs/app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const axios = require('axios').default;
+const Constants = require('./data/Constants');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,9 +8,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(Constants.HELLO_MESSAGE);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${Constants.HOSTNAME}:${Constants.PORT}/`);
 });
